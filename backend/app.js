6ada2e2eddb5944f5a155c0ff113417dc991cdd6
Ranjith <ranjith-3065@gmail.com>
2025-12-21 23:09:47 +0530

    auth/routes

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-
+const cookieparser = require('cookie-parser');
 const app = express();
 app.use(express.json());
+app.use(cookieparser());
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
+const path = require('path');
 
-app.use('/',(req,res)=>{
+app.set('views', path.join(__dirname, '../frontend/views'));
+
+
+const authrouter = require('./routes/auth.routes');
+
+app.use('/dashboard',(req,res)=>{
     console.log("requested:",req.url);
-    res.send("hello");
+    res.render('dashboard/index');
 })
+app.use('/',authrouter);
 
 module.exports = app;
