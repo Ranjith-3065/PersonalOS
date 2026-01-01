20d1b12790c902bf7abcce4ee656b5ab3a1f0f2f
Ranjith <ranjith20061917@gmail.com>
2026-01-01 22:25:48 +0530

    router auth updates

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -7,5 +7,8 @@ router.get('/signup',authcontroller.getsignup);
 router.post('/signup',authcontroller.postsignup);
 router.get('/login',authcontroller.getlogin);
 router.post('/login',authcontroller.postlogin);
+router.get('/forgotpass',authcontroller.getforgotpass);
+router.post('/resetpass',authcontroller.postresetpass);
+router.get('/passwordreset',authcontroller.getresetpass);
 
 module.exports =router;
