ae40ebeb58190019973bcf0293b2a0f35d60ae09
Ranjith <ranjith20061917@gmail.com>
2026-01-02 23:06:34 +0530

    changes

--- backend/routes/settings.routes.js
+++ backend/routes/settings.routes.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const settingscontroller = require('../controllers/settings.controller');
+
+//setings route
 router.get('/profile',settingscontroller.getprofile);
-router.get('/profile/data',settingscontroller.getprofiledata);
 router.get('/account',settingscontroller.getaccount);
 router.get('/preferences',settingscontroller.getpreferences);
 
+//settings/profile route
+router.get('/profile/data',settingscontroller.getprofiledata);
 router.patch('/profile/updatedata',settingscontroller.patchprofile);
+
+//settings/account route
 router.patch('/account/updatedata',settingscontroller.patchaccount);
 router.patch('/account/otp',settingscontroller.patchotp);
 
