6ff045918f693a06c12eaa0fc6bbdc891c6b116b
Ranjith <ranjith20061917@gmail.com>
2026-01-02 23:04:26 +0530

    changes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,11 +2,13 @@ const  express= require('express');
 const router = express.Router();
 const authcontroller = require('../controllers/auth.controller');
 
-
+//signup
 router.get('/signup',authcontroller.getsignup);
 router.post('/signup',authcontroller.postsignup);
+//login
 router.get('/login',authcontroller.getlogin);
 router.post('/login',authcontroller.postlogin);
+//forgotpass
 router.get('/forgotpass',authcontroller.getforgotpass);
 router.post('/resetpass',authcontroller.postresetpass);
 router.get('/passwordreset',authcontroller.getresetpass);
