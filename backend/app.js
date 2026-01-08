91eaad1ead9a6b05b5652fcc308eac8916b9f698
Ranjith <ranjith20061917@gmail.com>
2026-01-08 19:26:05 +0530

    layout method

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const cookieparser = require('cookie-parser');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 app.use(express.json());
 app.use(cookieparser());
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
+app.use(expressLayouts);
+app.set('layout', 'layouts/main');
+
 app.set('view engine','ejs');
 const path = require('path');
 
@@ -17,7 +21,12 @@ const authmiddleware = require('./middleware/auth.middleware')
 const seetingrouter = require('./routes/settings.routes');
 app.use('/personalOS/dashboard',authmiddleware.authtoken,(req,res)=>{
     console.log("requested:",req.user);
-    res.render('dashboard/index');
+    res.render('dashboard/index',{
+        title:'dashboard',
+        user:req.user,
+        scripts:['/js/dashboard.js'],
+        styles:['/css/dashboard.css']
+    });
 })
 app.use('/personalOS',authrouter);
 app.use('/personalOS',authmiddleware.authtoken,seetingrouter);
