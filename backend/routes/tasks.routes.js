6c406f27e2b31a367e86cffc010adb81a1f45e3e
Ranjith <ranjith20061917@gmail.com>
2026-01-19 23:21:38 +0530

    task progress router

--- backend/routes/tasks.routes.js
+++ backend/routes/tasks.routes.js
@@ -3,11 +3,20 @@ const router = express.Router();
 
 const taskscontroller = require('../controllers/tasks.controller');
 
+
+// dashboard router
 router.get('/tasksdashboard',taskscontroller.gettasksdashboard);
 router.post('/taskdashboardload',taskscontroller.posttasksdashboard);
+
+
+// task create router
 router.get('/tasks/create',taskscontroller.gettaskscreate);
 router.post('/tasks/create',taskscontroller.posttaskscreate);
-router.post('/tasks/progress',taskscontroller)
+
+// task progress router 
+router.post('/tasks/progress/today',taskscontroller.posttaskprogresstoday);
+router.post('/tasks/progress/upcoming',taskscontroller.posttaskprogressupcoming);
+router.post('/tasks/progress/completed',taskscontroller.posttaskprogresscompleted);
 
 
 module.exports = router;
