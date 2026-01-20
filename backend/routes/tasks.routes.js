75038a32a7f94970e573587e0c61fdae98f4a2dc
Ranjith <ranjith20061917@gmail.com>
2026-01-20 23:08:42 +0530

    task router changes

--- backend/routes/tasks.routes.js
+++ backend/routes/tasks.routes.js
@@ -14,8 +14,15 @@ router.get('/tasks/create',taskscontroller.gettaskscreate);
 router.post('/tasks/create',taskscontroller.posttaskscreate);
 
 // task progress router 
+router.get('/tasks/today',taskscontroller.gettaskstoday);
 router.post('/tasks/progress/today',taskscontroller.posttaskprogresstoday);
+
+//
+router.get('/tasks/upcoming',taskscontroller.gettasksupcoming);
 router.post('/tasks/progress/upcoming',taskscontroller.posttaskprogressupcoming);
+
+//
+router.get('/tasks/completed',taskscontroller.gettaskscompleted);
 router.post('/tasks/progress/completed',taskscontroller.posttaskprogresscompleted);
 
 
